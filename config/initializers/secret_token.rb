c0f7219e1883c3ff60807c7303e34b493321d272
Alberto Morales <amorales@opnet.com>
2013-01-07 10:27:08 -0500

    blank rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Orgchart::Application.config.secret_token = 'af3a1094da661beb306c4c30959491601d0cec8d0bcf6bd267fa4e3bf4cd39d3fbb48cdc298a389e85bbafffe5b3246da6a5ec97a000987c78e6ab6bc48e248a'
